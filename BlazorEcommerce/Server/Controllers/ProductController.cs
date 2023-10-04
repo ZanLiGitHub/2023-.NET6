e55509d9fde47c1343f7dc03d8e2f081500495eb
Zan Li <zan.li@cashflow-manager.com>
2023-10-04 16:29:27 +1030

    add Product

---
+++ BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorEcommerce.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public ProductController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProduct()
+        {
+            var product = await _dataContext.Products.ToListAsync();
+            return Ok(product);
+        }
+    }
+}

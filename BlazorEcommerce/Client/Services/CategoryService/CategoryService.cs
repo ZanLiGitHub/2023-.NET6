7eebba9dc792b77ca57fe0da6000208e247fb5c5
Zan Li <zan.li@cashflow-manager.com>
2023-10-23 15:34:01 +1030

    add category

---
+++ BlazorEcommerce/Client/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,25 @@
+﻿using BlazorEcommerce.Shared;
+using System.Net.Http.Json;
+
+namespace BlazorEcommerce.Client.Services.CategoryService
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly HttpClient _httpClient;
+        public CategoryService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public List<Category> Categories { get; set; }
+
+        public async Task GetCategories()
+        {
+            var result = await _httpClient.GetFromJsonAsync<ServiceResponse<List<Category>>>("api/Category");
+
+            if (result != null && result.Data != null)
+            {
+                Categories = result.Data;
+            }
+        }
+    }
+}
---
+++ BlazorEcommerce/Server/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorEcommerce.Server.Services.CategoryService
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly DataContext _datacontext;
+
+        public CategoryService(DataContext dataContext)
+        {
+            _datacontext = dataContext;
+        }
+
+        public async Task<ServiceResponse<List<Category>>> GetCategoriesAsync()
+        {
+            var categories = await _datacontext.Categories.ToListAsync();
+
+            return new ServiceResponse<List<Category>>
+            {
+                Data = categories
+            };
+        }
+    }
+}
